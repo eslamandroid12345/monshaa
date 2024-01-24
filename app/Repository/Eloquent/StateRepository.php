06f3afbb9049af78136e00fcc72fe30e3e157575
eslamandroid12345 <eslamemo457@gmail.com>
2024-01-24 15:55:11 +0200

    last update now 24-01-2024 3:55 PM

--- app/Repository/Eloquent/StateRepository.php
+++ app/Repository/Eloquent/StateRepository.php
@@ -24,13 +24,45 @@ class StateRepository extends Repository implements StateRepositoryInterface
     public function getAllStatusQuery(): LengthAwarePaginator
     {
 
-        return $this->model::query()
+        $query = $this->model::query();
+
+        $query->when(request()->has('real_state_address') && request('real_state_address') != null, function ($q)  {
+            $q->where('real_state_address', 'like', '%' . request()->input('real_state_address') . '%');
+        });
+
+        $query->when(request()->has('department') && request('department') != null, function ($q)  {
+            $q->where('department', '=',  request()->input('department'));
+        });
+
+        $query->when(request()->has('advertised_phone_number') && request('advertised_phone_number') != null, function ($q)  {
+            $q->where('advertised_phone_number', '=', request()->input('advertised_phone_number'));
+        });
+
+        $query->when(request()->has('lowest_price') && request()->has('highest_price') && request('lowest_price') != null && request('highest_price') != null, function ($q) {
+            $q->whereBetween('real_state_price', [request()->input('lowest_price'), request()->input('highest_price')]);
+        });
+
+        $query->when(request()->has('lowest_space') && request()->has('highest_space') && request('lowest_space') != null&& request('highest_space') != null, function ($q) {
+            $q->whereBetween('real_state_space', [request()->input('lowest_space'), request()->input('highest_space')]);
+        });
+
+        $query->when(request()->has('advertiser_type') && request('advertiser_type') != null, function ($q)  {
+            $q->where('advertiser_type', '=',  request()->input('advertiser_type'));
+        });
+
+        $query->when(request()->has('code') && request()->input('code') != null, function ($q)  {
+            $q->where('id', '=',  request()->input('code'));
+        });
+
+//        dd($query->toSql());
+        return $query
             ->latest()
             ->select(['*'])
             ->with(['user'])
             ->where('user_id','=',$this->authService->checkGuard())
             ->where('status','=','waiting')
             ->paginate(10);
+
 
     }
 
--- app/Repository/Eloquent/StateRepositoryInterface.php
+++ app/Repository/Eloquent/StateRepositoryInterface.php
@@ -3,9 +3,10 @@
 namespace App\Repository\Eloquent;
 
 use App\Repository\RepositoryInterface;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 interface StateRepositoryInterface extends RepositoryInterface
 {
 
-    public function getAllStatusQuery();
+    public function getAllStatusQuery() : LengthAwarePaginator;
 }
