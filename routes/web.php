ecda0a7f232b64318c6d5f8c786027a3aeb740e9
eslamandroid12345 <eslamemo457@gmail.com>
2024-03-22 13:33:44 +0200

    update routes

--- routes/web.php
+++ routes/web.php
@@ -18,20 +18,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-//    return TenantContract::query()
-//        ->where('company_id','=',21)
-//        ->whereDate('contract_date_to','=',Carbon::now()->subDays(3)->format('Y-m-d'))
-//        ->count();
-
-
-//    return TenantContract::query()
-//        ->where('company_id','=',21)
-//        ->whereDate('contract_date_to','=',Carbon::now()->addDays(3)->format('Y-m-d'))
-//        ->get();
-
-    return Company::query()->pluck('id')->toArray();
-//    return \App\Models\User::query()->pluck('id')->toArray();
-
-//    return view('welcome');
+    return view('welcome');
 });
 
