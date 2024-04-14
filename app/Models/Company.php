7838e0e27b2a74400696b7554e80a6296a944ca6
eslamandroid12345 <eslamemo457@gmail.com>
2024-04-14 14:24:07 +0200

    update default logo for company

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -21,7 +21,7 @@ class Company extends Model
                 if ($value !== null) {
                     return url($value);
                 }
-                return url('logo/logo.PNG');
+                return url('logo/logo.jpg');
             }
         );
     }
