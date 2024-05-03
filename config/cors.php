5d6977a6f40d891c60ce409070239ac5c2354ad5
eslamandroid12345 <eslamemo457@gmail.com>
2024-05-03 19:36:04 +0300

    update config now

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie','*'],
+    'paths' => ['api/*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://front.monshaa-crm.com'],
 
     'allowed_origins_patterns' => [],
 
