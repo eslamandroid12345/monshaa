9e1225bdd7d3da3a29fadebea906f5a1a9cb3202
eslamandroid12345 <eslamemo457@gmail.com>
2024-05-03 18:55:36 +0300

    update config now

--- config/cors.php
+++ config/cors.php
@@ -15,30 +15,20 @@ return [
     |
     */
 
-//    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-//
-//    'allowed_methods' => ['*'],
-//
-//    'allowed_origins' => ['*'],
-//
-//    'allowed_origins_patterns' => [],
-//
-//    'allowed_headers' => ['*'],
-//
-//    'exposed_headers' => [],
-//
-//    'max_age' => 0,
-//
-//    'supports_credentials' => false,
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'paths' => ['api/getList', 'api/getDetails/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*', '*.web.app'],
-    'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
-    'exposed_headers' => [],
-    'max_age' => 0,
-    'supports_credentials' => true,
 
+    'allowed_origins' => ['*'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
 
 ];
