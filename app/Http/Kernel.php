5501f50e8609c6a1b17bf3a7f35bda00c43bc32b
eslamandroid12345 <eslamemo457@gmail.com>
2024-05-04 14:14:29 +0300

    updat user service

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,7 +39,6 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Fruitcake\Cors\HandleCors::class,
 
 
         ],
@@ -48,8 +47,6 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Fruitcake\Cors\HandleCors::class,
-
         ],
     ];
 
--- config/cors.php
+++ config/cors.php
@@ -15,27 +15,11 @@ return [
     |
     */
 
-//    'paths' => ['api/*', 'sanctum/csrf-cookie','*'],
-//
-//    'allowed_methods' => ['*'],
-//
-//    'allowed_origins' => ['*'],
-//
-//    'allowed_origins_patterns' => [],
-//
-//    'allowed_headers' => ['*'],
-//
-//    'exposed_headers' => [],
-//
-//    'max_age' => 0,
-//
-//    'supports_credentials' => false,
+    'paths' => ['api/*', 'sanctum/csrf-cookie','*'],
 
-    'paths' => ['api/*'],
+    'allowed_methods' => ['*'],
 
-    'allowed_methods' => ['GET', 'POST'],
-
-    'allowed_origins' => ['https://front.monshaa-crm.com'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
