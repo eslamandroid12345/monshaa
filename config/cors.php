e15e704c9eb7da664c68e97d27d55d047a4a2cb2
eslamandroid12345 <eslamemo457@gmail.com>
2024-05-04 13:49:42 +0300

    update config

--- config/cors.php
+++ config/cors.php
@@ -15,20 +15,29 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie','*'],
+//    'paths' => ['api/*', 'sanctum/csrf-cookie','*'],
+//
+//    'allowed_methods' => ['*'],
+//
+//    'allowed_origins' => ['*'],
+//
+//    'allowed_origins_patterns' => [],
+//
+//    'allowed_headers' => ['*'],
+//
+//    'exposed_headers' => [],
+//
+//    'max_age' => 0,
+//
+//    'supports_credentials' => false,
 
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
+    'paths' => ['api/*'],
+    'allowed_methods' => ['GET', 'POST'],
+    'allowed_origins' => ['https://front.monshaa-crm.com'],
     'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
+    'allowed_headers' => ['Content-Type', 'Authorization'],
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => false,
 
 ];
