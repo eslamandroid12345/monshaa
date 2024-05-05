c475748a2ebb211a42d0e7d8fc4a515e657fddd5
eslamandroid12345 <eslamemo457@gmail.com>
2024-05-05 16:45:42 +0300

    update of cors in config now

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://front.monshaa-crm.com'],
+    'allowed_origins' => ['https://front.monshaa-crm.com','https://monshaa-crm.com'],
 
     'allowed_origins_patterns' => [],
 
